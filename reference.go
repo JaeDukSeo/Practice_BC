1b2073e7fff60746485f865cbc099b9c06a45700
jdseo <jae.duk.seo@ryerson.ca>
2017-03-19 16:48:07 -0400

    sds

--- reference.go
+++ reference.go
@@ -151,8 +151,8 @@ type SimpleChaincode struct {
 
 // read - query function to read key/value pair
 func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
-	var key, jsonResp string
-	var err error
+	// var key, jsonResp string
+	// var err error
 
 	if len(args) != 1 {
 		return nil, errors.New("Incorrect number of arguments. Expecting name of the key to query")
@@ -162,18 +162,16 @@ func (t *SimpleChaincode) read(stub shim.ChaincodeStubInterface, args []string) 
 	// key = args[0]
 	// valAsbytes, err := stub.GetState(key)
 
-	if err != nil {
-		jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
-		return nil, errors.New(jsonResp)
-	}
-	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+	// if err != nil {
+	// 	jsonResp = "{\"Error\":\"Failed to get state for " + key + "\"}"
+	// 	return nil, errors.New(jsonResp)
+	// }
 
     var personalInfo PersonalInfo
  	personalInfo = PersonalInfo{"Varun", "Ojha", "dob", "varun@gmail.com", "9999999999"}
  	bytes, err := json.Marshal (&personalInfo)
 
-
-	return bytes, nil
+	return bytes, err
 }
 
 // write - invoke function to write key/value pair
@@ -217,7 +215,12 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	if function == "read" { 
 		//read a variable - function = read , args - given in string argument
 		// return nil, errors.New("This is not an error!!!! \nMy First Block Chain Application to update ledger!!")
-		return t.read(stub, args)
+	    var personalInfo PersonalInfo
+		personalInfo = PersonalInfo{"Varun", "Ojha", "dob", "varun@gmail.com", "9999999999"}
+		bytes, err := json.Marshal (&personalInfo)
+		fmt.Println(err)
+		return bytes,nil
+		// return t.read(stub, args)
 	}
 
 	return nil, errors.New("Received unknown function query: " + function)
